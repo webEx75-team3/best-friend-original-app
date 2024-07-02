63def49b49490b51b42d46730a7927e636af037c
Kota1456 <ddatank1627@gmail.com>
2024-07-02 10:57:27 +0900

    おためし

---
+++ best-friend-original-app-Mattsun/research-site/src/pages/NewPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewPost = () => {
+    return (
+        <div>
+            <h2>新規投稿ページ</h2>
+            <p>ここに投稿内容を入力してください</p>
+        </div>
+    );
+};
+
+export default NewPost;
---
+++ best-friend-original-app-Mattsun/research-site/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div>
+            <h2>検索ページ</h2>
+            <p>ここに検索機能を実装します</p>
+        </div>
+    );
+};
+
+export default Search;
