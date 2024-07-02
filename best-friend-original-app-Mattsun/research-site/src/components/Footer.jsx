f4569cc43006fa250f57b439a1aa17a8bcd0d047
Kota1456 <ddatank1627@gmail.com>
2024-07-02 13:01:02 +0900

    変更

--- best-friend-original-app-Mattsun/research-site/src/components/Footer.jsx
+++ best-friend-original-app-Mattsun/research-site/src/components/Footer.jsx
@@ -8,9 +8,11 @@ const Footer = () => {
             <ul>
                 <li>殿堂入りするとここに掲載</li>
                 <li>いいね数の変動で形が変わります</li>
+                <p>ありがとう</p>
             </ul>
         </footer>
     );
 };
 
 export default Footer;
+
