d071b6ef6e804bb73081fa6cda6915cf19794ff6
Mattsun20030101 <arashi5lovers@gmail.com>
2024-07-16 11:55:10 +1000

    topページが完成しました

--- app/component/MapComponent.jsx
+++ app/component/MapComponent.jsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { GoogleMap, LoadScript, StandaloneSearchBox,} from '@react-google-maps/api';
+import React, { useCallback, useRef, useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, StandaloneSearchBox, Marker } from '@react-google-maps/api';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -14,34 +15,83 @@ const center = {
 
 const Map = () => {
   const [map, setMap] = useState(null);
-  const [places, setPlaces] = useState([]);
+  const [markers, setMarkers] = useState([]);
   const searchBox = useRef(null);
+  const [customIcon, setCustomIcon] = useState(null);
+  const [isApiLoaded, setIsApiLoaded] = useState(false);
 
-  const onLoad = useCallback((map) => {
-    setMap(map);
+  const onMapLoad = useCallback((mapInstance) => {
+    setMap(mapInstance);
   }, []);
 
   const onPlacesChanged = () => {
     const places = searchBox.current.getPlaces();
-    setPlaces(places);
     if (places.length > 0) {
       const place = places[0];
       const location = place.geometry.location;
+      const newMarker = { lat: location.lat(), lng: location.lng() };
       map.panTo(location);
+      setMarkers((prevMarkers) => [...prevMarkers, newMarker]);
     }
   };
 
+  useEffect(() => {
+    if (isApiLoaded) {
+      setCustomIcon({
+        url: 'https://path-to-your-icon.png', // カスタムアイコンのURL
+        scaledSize: new window.google.maps.Size(50, 50), // アイコンのサイズを調整
+        origin: new window.google.maps.Point(0, 0), // アイコンの起点
+        anchor: new window.google.maps.Point(25, 25), // アイコンのアンカー
+      });
+    }
+  }, [isApiLoaded]);
+
+  const handleApiLoad = () => {
+    setIsApiLoaded(true);
+  };
+
+  const Marker = (options) => {
+    const [marker, setMarker] = React.useState();
+  
+    React.useEffect(() => {
+      if (!marker) {
+        setMarker(new google.maps.Marker());
+      }
+  
+      // remove marker from map on unmount
+      return () => {
+        if (marker) {
+          marker.setMap(null);
+        }
+      };
+    }, [marker]);
+    React.useEffect(() => {
+      if (marker) {
+        marker.setOptions(options);
+      }
+    }, [marker, options]);
+    return null;
+  };
+  
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyBk34MJtHoEDYT7xu9SW4Jl4ctJZP9JZ-U" // ここにAPIキーを入力
       libraries={libraries}
+      onLoad={handleApiLoad}
     >
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={center}
         zoom={10}
-        onLoad={onLoad}
+        onLoad={onMapLoad}
       >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={customIcon}
+          />
+        ))}
         <StandaloneSearchBox
           onLoad={(ref) => (searchBox.current = ref)}
           onPlacesChanged={onPlacesChanged}
--- app/routes/top.jsx
+++ app/routes/top.jsx
@@ -1,29 +1,44 @@
-import React from "react";
-//import "./top.css";  // CSSファイルをインポート
+import React, { useState, useEffect } from 'react';
+import '../stylesheets/top.css'; 
 
-//export function links() {
-//  return [{ rel: "stylesheet", href: "./top.css" }];
-//}
+const images = [
+  'https://www.nta.co.jp/media/tripa/static_contents/nta-tripa/item_images/images/000/181/707/medium/58caabf7-26f3-4bae-9a13-b1353f25f175.jpg?1659483469',
+  'https://www.rere.jp/beginners/uploads/2018/12/5875dcfc67437790d678e15db9fe85c3_s.jpg',
+  'https://static.retrip.jp/article/67263/images/67263960315e2-33f2-499f-aa00-9588bfb26a8a_l.jpg',
+  'https://s3-ap-northeast-1.amazonaws.com/cdn.tabi-biyori.jp/production/posts/eyecatches/000/000/802/original.jpg?'
+];
 
 const Top = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalid = setInterval(() => {
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(intervalid);
+  }, []);
+
+  const handleExpolerClick = () => {
+    window.location.href = '/index';
+  }
+
   return (
     <div className="container">
-      <header className="header">ずっともページ</header>
-      <div className="main-content">
-        <div className="box">新規投稿</div>
-        <div className="box">検索</div>
-        <div className="footer">
-          <p>お知らせ</p>
-          <ul>
-            <li>殿堂入れするとここに掲載</li>
-            <li>いいねの数の変動で形が変わります</li>
-            <li>気軽に投稿よろしくお願いします!</li>
-          </ul>
-        </div>
+      <div className="header">日本新規開拓</div>
+      <div className="banner">
+        <span className="banner-text">日本の新たな穴場スポットを発掘しよう！</span>
+      </div>
+      <div className="image-container">
+        <img src={images[currentImageIndex]} alt="Scenery"/>
+      </div>
+      <div className="button-container">
+        <button className="explore-button" onClick={handleExpolerClick}>発掘</button>
       </div>
     </div>
   );
 };
 
 export default Top;
+
 
